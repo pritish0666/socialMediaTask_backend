81e418e1976fe46b96ae4285d70ebb99656262d3
Pritish <pritishpatra06@gmail.com>
2024-10-12 01:16:07 +0530

    bug fix

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const cors = require("cors");
 
 const app = express();
 dotenv.config();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
